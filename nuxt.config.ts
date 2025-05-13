68ac99ca911df3e856cf1267a91f74c3a61e3512
Rauljp16 <raul_astra16@hotmail.com>
2025-05-13 20:33:25 +0200

    refactor

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,21 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'Car Fácil',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'theme-color', content: '#ffffff' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', href: '/favicon.ico' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ]
+    }
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
